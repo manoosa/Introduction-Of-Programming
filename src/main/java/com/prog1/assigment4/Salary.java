8a9672cb33e6f69bf20168be33958254340bac9a
Marwa Saleh <extern.msaleh@novomind.com>
2020-06-29 00:12:03 +0200

    [Assigment4] question1&2

---
+++ src/main/java/com/prog1/assigment4/Salary.java
@@ -0,0 +1,38 @@
+package com.prog1.assigment4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Salary {
+  public static void main(String[] args) throws IOException {
+    InputStreamReader inStream = new InputStreamReader(System.in);
+    BufferedReader stdin = new BufferedReader(inStream);
+    String inData;
+    double salary;
+    int rating;
+    Double newSalary = null;
+    
+    System.out.print("Please enter the annual salary: ");
+    inData = stdin.readLine();
+    salary = Double.parseDouble(inData);
+    
+    System.out.print("Please enter the rating: ");
+    inData = stdin.readLine();
+    rating = Integer.parseInt(inData);
+    
+    if (rating == 1) {
+      newSalary = 1.06 * salary;
+    } else if (rating == 2) {
+      newSalary = 1.04 * salary;
+    } else if (rating == 3) {
+      newSalary = 1.015 * salary;
+    }
+    
+    if (newSalary != null) {
+      System.out.print("New Salary: " + newSalary);
+    } else {
+      System.out.print("Invalid rating");
+    }
+  }
+}
